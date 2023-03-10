cbdef1781f0b674b41e44e86d2e1721f1cb2ec77
YendisFish <brintonsid@gmail.com>
2023-03-09 16:24:22 -0800

    Addition of mappings object

--- Backend/Types/AppConfig.cs
+++ Backend/Types/AppConfig.cs
@@ -4,9 +4,12 @@ public class AppConfig
 {
     public List<string> Prefixes = new List<string>();
     public string RootDir { get; set; }
-    public AppConfig(string[] prefixes, string rootdir = "./")
+    public List<Mapping> Mappings { get; set; }
+
+    public AppConfig(string[] prefixes, List<Mapping> maps, string rootdir = "./")
     {
         Prefixes.AddRange(prefixes);
         RootDir = rootdir;
+        Mappings = maps;
     }
 }
