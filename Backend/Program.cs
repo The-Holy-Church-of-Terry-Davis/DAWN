195fd4aba88af8e4c4ecc49c1178cda9f39b9087
YendisFish <brintonsid@gmail.com>
2023-03-11 10:02:15 -0800

    Deprecation of attempted CLI, server parallel

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -1,5 +1,4 @@
-﻿using Dawn.CLI;
-using Dawn.Types;
+﻿using Dawn.Types;
 using Dawn.Server;
 using Newtonsoft.Json;
 
@@ -7,15 +6,9 @@ namespace Dawn;
 
 class Program
 {
-    public static async Task Main(params string[] args)
+    public static void Main()
     {
-        if(args.Length > 0)
-        {
-            CliManger cli = new CliManger(args);
-            await cli.ProcessArgs();
-        } else {
-            AppConfig? cfg = JsonConvert.DeserializeObject<AppConfig?>(File.ReadAllText("./appconfig.json"));
-            WebServer srv = new WebServer(cfg ?? new(new(1), new(), "./"));
-        }
+        AppConfig? cfg = JsonConvert.DeserializeObject<AppConfig?>(File.ReadAllText("./appconfig.json"));
+        WebServer srv = new WebServer(cfg ?? new(new(1), new(), "./"));
     }
 }
