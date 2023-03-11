fdb1f34dfee6e5c02b4717df8e407e1f3edcd3ed
YendisFish <brintonsid@gmail.com>
2023-03-11 09:31:06 -0800

    Partially tested version of CLI

--- Backend/CLI/CliManager.cs
+++ Backend/CLI/CliManager.cs
@@ -27,14 +27,16 @@ public class CliManger
                             DirectoryInfo inf = Directory.CreateDirectory(args[i + 1]);
                             Directory.SetCurrentDirectory(inf.FullName);
 
-                            File.Create("./appconfig.json");
-                            File.WriteAllText(args[i + 1], JsonConvert.SerializeObject(AppConfig.GetDefaultAppConfig(args[i + 1])));
+                            File.Create("./appconfig.json").Close();
+                            await Task.Delay(1);
+                            File.WriteAllText("./appconfig.json", JsonConvert.SerializeObject(AppConfig.GetDefaultAppConfig(args[i + 1])));
 
                             HttpClient cli = new HttpClient();
                             byte[] buf = await cli.GetByteArrayAsync("https://github.com/The-Holy-Church-of-Terry-Davis/DAWN/releases/tag/2023-3-11");
                             
                             FileStream fs = File.Create("DAWN.exe");
                             await fs.WriteAsync(buf, 0, buf.Length);
+                            fs.Close();
 
                             i++;
                             break;
