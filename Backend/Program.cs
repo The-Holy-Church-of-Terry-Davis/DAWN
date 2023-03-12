4dca21e09c7a52ca466b0258b3717f992c7aec93
YendisFish <brintonsid@gmail.com>
2023-03-11 21:39:45 -0800

    More removal of code bloat

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -7,21 +7,6 @@ namespace Dawn;
 
 class Program
 {
-    private static string whitespaceCounter(int len, string str) {
-        try {
-            int whitespace = 44 - len;
-            string toReturn = $"{str}" + String.Concat(Enumerable.Repeat(" ", whitespace));
-            return toReturn;
-        } catch (ArgumentOutOfRangeException) {
-            try {
-                string toReturn = $"{String.Concat(Enumerable.Repeat(' ', 44))}║\n║          {str}{String.Concat(Enumerable.Repeat(' ', 60 - len))}";
-                return toReturn;
-            } catch (ArgumentOutOfRangeException) {
-                string toReturn = $"{String.Concat(Enumerable.Repeat(' ', 44))}║\n║          --unavailable, project name too long!{String.Concat(Enumerable.Repeat(' ', 23))}";
-                return toReturn;
-            }
-        }
-    }
     public static void Main()
     {
         Console.WriteLine($"{Colors.setColor(ConsoleColor.Cyan)}[-] Prepping DAWN");
