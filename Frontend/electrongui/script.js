da2840d877734ddccc0b0f23e2528321c7944a64
QAEZZ <russian.devv@gmail.com>
2023-03-14 17:50:38 -0500

    end of buttons

--- Frontend/electrongui/script.js
+++ Frontend/electrongui/script.js
@@ -2,6 +2,10 @@
 let submitButton = document.getElementById("projNameButton");
 let projNameLabel = document.getElementById("projName");
 let projNameTextInput = document.getElementById("projNameTextInput");
+let runningOS = document.getElementById("runningOS");
+
+const osWindowsButton = document.getElementById("osWindowsButton");
+const osLinuxButton = document.getElementById("osLinuxButton");
 
 function getTextInput(element) { 
     val = element.value.replace(/[^a-zA-Z0-9]/g, "").replace(/\s/g, "");
@@ -13,4 +17,18 @@ function getTextInput(element) {
 
 function updateProjNameLabel() {
     projNameLabel.firstChild.data = getTextInput(projNameTextInput);
+}
+
+function runningOSSelect(operatingSystem) {
+    if (operatingSystem == "Windows") {
+        osWindowsButton.dataset.selected = true;
+        osLinuxButton.dataset.selected = false;
+    } else {
+        osLinuxButton.dataset.selected = true;
+        osWindowsButton.dataset.selected = false;
+    }
+
+    
+    runningOS.firstChild.data = operatingSystem;
+
 }
